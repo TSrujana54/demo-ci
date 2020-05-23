53e8bc7815eb3a9c5f0af761337d68406ec554ce
stiruna9 <stiruna9@in.ibm.com>
2020-05-23 11:45:58 +0530

    add files

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello World from GitLab Runner");
+      System.out.println("Triggering a new Build from Dell Bangalore 29th Jan 2020!!!");
+    }
+}
